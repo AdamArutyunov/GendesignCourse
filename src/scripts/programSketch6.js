9791ebb06d2967f506e4c687c6dfba15578aa3df
illus0r <zor667@gmail.com>
2022-11-22 23:55:37 +0300

    Updated subdivision microsketch to react on entering viewport

--- src/scripts/programSketch6.js
+++ src/scripts/programSketch6.js
@@ -2,19 +2,19 @@ export const sk6 = (container, ff) => {
   return p => {
     let squares = [];
     let i = 0;
+    let inViewportPrev;
 
     let reset = () => {
-      // console.log('resize');
       i = 0;
       squares = [];
       squares.push([0, 0, container.offsetWidth, container.offsetHeight]);
       p.loop();
+      p.background(200);
     };
 
     p.setup = () => {
-      // console.log('draw');
-      p.createCanvas(100, 100);
-      p.stroke(255);
+      p.createCanvas(0, 0);
+      p.noStroke();
       ff.resize();
       reset();
     };
@@ -24,22 +24,25 @@ export const sk6 = (container, ff) => {
     };
 
     p.draw = () => {
-      if (i > 8) {
-        p.noLoop();
+      if (inViewportPrev === false && ff.inViewport === true) {
+        reset();
+      }
+      inViewportPrev = ff.inViewport;
+      if (i > 12) {
         return;
       } else {
         let newSquares = [];
         for (let s of squares) {
-          if (p.random() < 0.8 || i < 2) {
-            newSquares.push([s[0], s[1], s[2] / 2, s[3] / 2]);
-            newSquares.push([s[0], s[1] + s[3] / 2, s[2] / 2, s[3] / 2]);
-            newSquares.push([s[0] + s[2] / 2, s[1], s[2] / 2, s[3] / 2]);
-            newSquares.push([
-              s[0] + s[2] / 2,
-              s[1] + s[3] / 2,
-              s[2] / 2,
-              s[3] / 2,
-            ]);
+          if (p.random() < 0.8 || i < 4) {
+            if (s[2] > s[3]) {
+              // horiz
+              newSquares.push([s[0], s[1], s[2] / 2, s[3]]);
+              newSquares.push([s[0] + s[2] / 2, s[1], s[2] / 2, s[3]]);
+            } else {
+              // vert
+              newSquares.push([s[0], s[1], s[2], s[3] / 2]);
+              newSquares.push([s[0], s[1] + s[3] / 2, s[2], s[3] / 2]);
+            }
           } else {
             let randomColor = p.random(200, 255);
             p.fill(randomColor);
@@ -47,9 +50,7 @@ export const sk6 = (container, ff) => {
           }
         }
         squares = newSquares;
-        // console.log('squares:', squares);
         i++;
-        // }
       }
     };
   };
